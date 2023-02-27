f6b817829cb0c38c4d62fcdd42bc78cd9ac9b07a
Adam Kareen <me@akareen.com>
2023-02-27 17:56:30 +1100

    removed all files to start fresh

--- Chapter 1 - Introduction/Exercise 1.02.c
+++
@@ -1,7 +0,0 @@
-# include <stdio.h>
-
-//Creates an unknown escape sequence warning
-
-int main() {
-    printf("Hello, \cworld!\n");
-}
--- Chapter 2 - Types, Operators and Expressions/Exercise 2.06.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main() {
-
-}
-
-int setbits(int source, int sourcePos, int replace, int replacePos) {
-	
-}
